217da61d230735b47d4e1fd0ffdddb2fb9d342b6
dawit101 <130009377+dawit101@users.noreply.github.com>
2023-04-06 23:19:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Augmented Assignment Operator
+#AAO = performing arithmetic or bitwise operations and assigning the result back to the same variable.
+
+some_value = 5
+aValue = 3
+
+some_value += 2  #some_value = some_value + 2 = 7
+aValue -= 2  #aValue = aValue - 2 = 1
+
+print(some_value)
+print(aValue)
+
